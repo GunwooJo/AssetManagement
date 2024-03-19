3516f12b272769e62ab9f171b50290dc3df61fb3
GunwooJo <kanggi1997@naver.com>
2024-03-19 10:54:48 +0900

    connectedId 발급 서비스 코드

---
+++ backend/src/main/java/kangnamuniv/assetmanagement/service/MemberService.java
@@ -0,0 +1,61 @@
+package kangnamuniv.assetmanagement.service;
+
+import kangnamuniv.assetmanagement.ApiRequest;
+import kangnamuniv.assetmanagement.CommonConstant;
+import kangnamuniv.assetmanagement.RSAUtil;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class MemberService {
+
+    public String generateConnectedId(String businessType, String loginType, String organization, String id, String password, String birthday) {
+        String urlPath = "https://development.codef.io/v1/account/create";
+
+        HashMap<String, Object> bodyMap = new HashMap<String, Object>();
+        List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+
+        HashMap<String, Object> accountMap = new HashMap<String, Object>();
+        accountMap.put("countryCode",	"KR");
+        accountMap.put("businessType",	businessType);
+        accountMap.put("clientType",  	"P");
+        accountMap.put("organization",	organization);
+        accountMap.put("loginType",  	loginType);
+        accountMap.put("id", id);
+        accountMap.put("birthday", birthday);
+
+        /**	password RSA encrypt */
+        try {
+            accountMap.put("password", RSAUtil.encryptRSA(password, CommonConstant.PUBLIC_KEY));
+        } catch(NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
+
+        list.add(accountMap);
+
+        bodyMap.put("accountList", list);
+
+        // CODEF API 호출
+        String result = null;
+
+        try {
+            result = ApiRequest.request(urlPath, bodyMap);
+        } catch (IOException | InterruptedException | ParseException e) {
+            e.printStackTrace();
+        } finally {
+            return result;
+        }
+
+    }
+}
