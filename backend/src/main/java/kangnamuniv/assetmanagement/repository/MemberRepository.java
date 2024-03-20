56458f81cd6f722d90bd0ba4132058d9662e4636
GunwooJo <kanggi1997@naver.com>
2024-03-21 00:48:09 +0900

    회원가입 repository 작성

--- backend/src/main/java/kangnamuniv/assetmanagement/repository/MemberRepository.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/repository/MemberRepository.java
@@ -1,2 +1,20 @@
-package kangnamuniv.assetmanagement.repository;public class MemberRepository {
+package kangnamuniv.assetmanagement.repository;
+
+import jakarta.persistence.EntityManager;
+import kangnamuniv.assetmanagement.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
 }
