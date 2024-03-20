ec8089215b2c8b4a72c8ba14635fef25cab0df4a
GunwooJo <kanggi1997@naver.com>
2024-03-21 01:12:40 +0900

    회원목록 아이디로 찾기 repository 코드

--- backend/src/main/java/kangnamuniv/assetmanagement/repository/MemberRepository.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/repository/MemberRepository.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional
 @RequiredArgsConstructor
@@ -16,5 +18,11 @@ public class MemberRepository {
     public Long save(Member member) {
         em.persist(member);
         return member.getId();
+    }
+
+    public List<Member> findByLoginId(String login_id) {
+        return em.createQuery("select m from Member m where m.login_id = :login_id", Member.class)
+                .setParameter("login_id", login_id)
+                .getResultList();
     }
 }
