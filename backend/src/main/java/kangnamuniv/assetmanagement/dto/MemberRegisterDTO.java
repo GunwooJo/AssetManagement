a9c8d78e6a28b8fcdd24fe0a5998854950ec89af
GunwooJo <kanggi1997@naver.com>
2024-03-21 00:45:16 +0900

    DTO 정의: MemberRegisterDTO

---
+++ backend/src/main/java/kangnamuniv/assetmanagement/dto/MemberRegisterDTO.java
@@ -0,0 +1,15 @@
+package kangnamuniv.assetmanagement.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberRegisterDTO {
+
+    private String login_id;
+
+    private String password;
+
+    private String name;
+
+    private int age;
+}
