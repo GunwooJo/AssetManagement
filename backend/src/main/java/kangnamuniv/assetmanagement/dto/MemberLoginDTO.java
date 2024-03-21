95dcd69229d3275e286f50178de692dd23dab524
GunwooJo <kanggi1997@naver.com>
2024-03-21 16:49:31 +0900

    dto to entity 변환 메서드 작성

--- backend/src/main/java/kangnamuniv/assetmanagement/dto/MemberLoginDTO.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/dto/MemberLoginDTO.java
@@ -1,9 +1,17 @@
 package kangnamuniv.assetmanagement.dto;
 
+import kangnamuniv.assetmanagement.entity.Member;
 import lombok.Getter;
 
 @Getter
 public class MemberLoginDTO {
     private String login_id;
     private String password;
+
+    public static Member toMember(MemberLoginDTO memberLoginDTO) {
+        Member member = new Member();
+        member.setLogin_id(memberLoginDTO.getLogin_id());
+        member.setPassword(memberLoginDTO.getPassword());
+        return member;
+    }
 }
