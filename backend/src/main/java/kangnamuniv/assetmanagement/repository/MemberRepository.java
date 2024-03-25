ded35be3ad660f9dfd43532f6e9eee2554d126da
GunwooJo <kanggi1997@naver.com>
2024-03-25 17:59:58 +0900

    connected_id db에 저장하는 메서드

--- backend/src/main/java/kangnamuniv/assetmanagement/repository/MemberRepository.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package kangnamuniv.assetmanagement.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import kangnamuniv.assetmanagement.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -24,5 +25,18 @@ public class MemberRepository {
         return em.createQuery("select m from Member m where m.login_id = :login_id", Member.class)
                 .setParameter("login_id", login_id)
                 .getResultList();
+    }
+
+    public void saveConnectedId(String login_id, String connected_id) {
+
+        try {
+            Member foundMember = em.createQuery("select m from Member m where m.login_id = :login_id", Member.class)
+                    .setParameter("login_id", login_id).getSingleResult();
+            foundMember.setConnected_id(connected_id);
+
+        } catch (NoResultException e) {
+            e.printStackTrace();
+        }
+
     }
 }
