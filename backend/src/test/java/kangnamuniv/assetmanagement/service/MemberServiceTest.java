96fa1a4547a20dcca4f133ceb27e303923e92b15
GunwooJo <kanggi1997@naver.com>
2024-03-25 16:37:09 +0900

    계정 등록과 추가코드 분리(메서드 오버로딩)

--- backend/src/test/java/kangnamuniv/assetmanagement/service/MemberServiceTest.java
+++ backend/src/test/java/kangnamuniv/assetmanagement/service/MemberServiceTest.java
@@ -2,15 +2,13 @@ package kangnamuniv.assetmanagement.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MemberServiceTest {
 
     MemberService memberService = new MemberService();
 
     @Test
     void generateConnectedId() {
-        String result = memberService.generateConnectedId("BK", "1", "0081", "kanggi1997", "temp", "971101");
+        String result = memberService.addAccount("BK", "1", "0081", "kanggi1997", "temp", "971101");
 
         System.out.println("result = " + result);
     }
