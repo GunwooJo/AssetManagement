71aa43177d0a28577751dc7eb73d0470935edada
GunwooJo <kanggi1997@naver.com>
2024-03-27 19:37:11 +0900

    secretKey 주입을 위해 static 제거.

--- backend/src/main/java/kangnamuniv/assetmanagement/util/JwtUtil.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/util/JwtUtil.java
@@ -5,21 +5,27 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
 
     @Value("${jwt.secret}")
-    private static String secretKey;
+    private String secretKey;
 
-    private static Key getSigningKey() {
+    private Key getSigningKey() {
+        if (secretKey == null) {
+            throw new IllegalStateException("Secret key must not be null");
+        }
         // Assumes your key is Base64-encoded; decode if necessary
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
 
-    public static String generateToken(String login_id) {
+    public String generateToken(String login_id) {
         long expirationTime = 1000 * 60 * 30; // Token validity 30 minutes.
 
         return Jwts.builder()
@@ -30,7 +36,7 @@ public class JwtUtil {
                 .compact();
     }
 
-    public static boolean validateToken(String token) {
+    public boolean validateToken(String token) {
         try {
             // This line will throw an exception if it is not a signed JWS (as expected)
             Jwts.parserBuilder()
@@ -46,7 +52,7 @@ public class JwtUtil {
 
     // Method to extract login_id from token
     //토큰을 검증하고 검증이 완료되면 이 메서드를 통해 요청자의 이름을 얻어내고, Role, Permission 등 다양한 정보를 얻어서 특정 자료에 접근 가능하게 할 것인지 결정할 수 있다.
-    public static String getLoginIdFromToken(String token) {
+    public String getLoginIdFromToken(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
                 .build()
