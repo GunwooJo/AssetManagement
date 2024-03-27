1210bdcfb872d5298715a45d056d420eab346a33
GunwooJo <kanggi1997@naver.com>
2024-03-27 19:33:10 +0900

    불필요한 테스트 코드 제거

--- backend/src/main/java/kangnamuniv/assetmanagement/AssetmanagementApplication.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/AssetmanagementApplication.java
@@ -12,34 +12,8 @@ import java.util.List;
 
 @SpringBootApplication
 public class AssetmanagementApplication {
-
-    public static void showAccounts() {
-        String urlPath = "https://development.codef.io/v1/kr/bank/p/account/account-list";
-
-        HashMap<String, Object> bodyMap = new HashMap<String, Object>();
-        List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
-
-        HashMap<String, Object> accountMap2 = new HashMap<String, Object>();
-        accountMap2.put("organization",	"0081");
-        accountMap2.put("connectedId", "5cKBSS.ZkHtbRzHCJeS3Hh");
-//        accountMap2.put("birthDate", "19971101");
-
-        String result = null;
-        try {
-            result = ApiRequest.request(urlPath, accountMap2);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("result = " + result);
-    }
 	public static void main(String[] args) {
 		SpringApplication.run(AssetmanagementApplication.class, args);
-
-//        showAccounts();
 	}
 
 }
