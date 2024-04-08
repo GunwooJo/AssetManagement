e5d3e113e16376b2c0eb51927fd8e94da2a28150
GunwooJo <kanggi1997@naver.com>
2024-04-08 17:17:05 +0900

    영속성 전이, 고아객체 설정

--- backend/src/main/java/kangnamuniv/assetmanagement/entity/StockAccount.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/entity/StockAccount.java
@@ -1,5 +1,6 @@
 package kangnamuniv.assetmanagement.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -18,7 +19,7 @@ public class StockAccount extends Account{
     @Column(name = "deposit_received", precision=16, scale=2, nullable = false)
     private BigDecimal depositReceived;
 
-    @OneToMany(mappedBy = "stockAccount")
+    @OneToMany(mappedBy = "stockAccount", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Stock> stockList = new ArrayList<>();
 
 }
