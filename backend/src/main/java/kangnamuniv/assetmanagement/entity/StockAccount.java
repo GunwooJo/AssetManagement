0e4710ea307d0e30af402de3db98870b891065f8
GunwooJo <kanggi1997@naver.com>
2024-04-08 16:26:22 +0900

    feat: 양방향 연관관계 설정

--- backend/src/main/java/kangnamuniv/assetmanagement/entity/StockAccount.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/entity/StockAccount.java
@@ -2,15 +2,23 @@ package kangnamuniv.assetmanagement.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
 public class StockAccount extends Account{
 
+    @Setter
     @Column(name = "deposit_received", precision=16, scale=2, nullable = false)
     private BigDecimal depositReceived;
+
+    @OneToMany(mappedBy = "stockAccount")
+    private List<Stock> stockList = new ArrayList<>();
+
 }
