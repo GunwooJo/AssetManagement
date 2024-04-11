8d45e7c47bba6a922e0e41d043d1a36a08923411
GunwooJo <kanggi1997@naver.com>
2024-04-11 09:15:24 +0900

    DB 저장 관련 에러 핸들링 추가

--- backend/src/main/java/kangnamuniv/assetmanagement/entity/BankAccount.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/entity/BankAccount.java
@@ -13,7 +13,4 @@ public class BankAccount extends Account{
     @Column(precision=16, scale=2, nullable = false)
     private BigDecimal balance;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private AccountCurrency accountCurrency; //통화코드 KRW: 한국원화, JPY: 일본 엔, USD: 미국 달러, EUR: 유로
 }
