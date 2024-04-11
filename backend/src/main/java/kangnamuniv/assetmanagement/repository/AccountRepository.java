b7e19c615353866c85e0219e80f4122c1fc61724
GunwooJo <kanggi1997@naver.com>
2024-04-11 18:01:30 +0900

    DB저장 시 예외처리, 통화별 원화 계산 메서드, 계좌번호로 은행계좌 찾기

--- backend/src/main/java/kangnamuniv/assetmanagement/repository/AccountRepository.java
+++ backend/src/main/java/kangnamuniv/assetmanagement/repository/AccountRepository.java
@@ -2,39 +2,50 @@ package kangnamuniv.assetmanagement.repository;
 
 import jakarta.persistence.EntityManager;
 import kangnamuniv.assetmanagement.entity.*;
+import kangnamuniv.assetmanagement.util.CommonConstant;
 import kangnamuniv.assetmanagement.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Repository
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class AccountRepository {
 
     private final EntityManager em;
     private final MemberRepository memberRepository;
     private final JwtUtil jwtUtil;
 
-    public void saveBankAccount(Member member, String accountNumber, String organization, String businessType, AccountCurrency accountCurrency, BigDecimal balance) {
+    public void saveBankAccount(Member member, String accountNumber, String organization, String businessType, BigDecimal balance, String accountName) throws Exception {
 
-        BankAccount bankAccount = new BankAccount();
-        bankAccount.setAccountNumber(accountNumber);
-        bankAccount.setOrganization(organization);
-        bankAccount.setBusinessType(businessType);
-        bankAccount.setAccountCurrency(accountCurrency);
-        bankAccount.setBalance(balance);
-        bankAccount.setMember(member);
+        try {
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.setAccountNumber(accountNumber);
+            bankAccount.setOrganization(organization);
+            bankAccount.setBusinessType(businessType);
+            bankAccount.setBalance(balance);
+            bankAccount.setMember(member);
+            bankAccount.setAccountName(accountName);
 
-        em.persist(bankAccount);
+            em.persist(bankAccount);
+
+        } catch (Exception e) {
+            log.error("BankAccount 저장 중 에러: " + e.getMessage());
+            throw new Exception(e);
+        }
+
     }
 
-    public void saveStockAccount(Member member, String accountNumber, String organization, String businessType, BigDecimal depositReceived) {
+    public StockAccount saveStockAccount(Member member, String accountNumber, String organization, String businessType, BigDecimal depositReceived, String accountName) {
 
         StockAccount stockAccount = new StockAccount();
         stockAccount.setMember(member);
@@ -42,8 +53,10 @@ public class AccountRepository {
         stockAccount.setOrganization(organization);
         stockAccount.setBusinessType(businessType);
         stockAccount.setDepositReceived(depositReceived);
+        stockAccount.setAccountName(accountName);
 
         em.persist(stockAccount);
+        return stockAccount;
     }
 
     public List<StockAccount> findStockAccountListByLoginId(String login_id) {
@@ -71,7 +84,6 @@ public class AccountRepository {
                 .getSingleResult();
 
         foundBankAccount.setBalance(new BigDecimal(balance));
-        foundBankAccount.setAccountCurrency(accountCurrency);
     }
 
     // db에 저장된 은행 계좌의 organization들 찾기.
@@ -85,5 +97,32 @@ public class AccountRepository {
             organizations.add(foundAccount.getOrganization());
         }
         return organizations;
+    }
+
+    private BigDecimal exchangeMoney(AccountCurrency accountCurrency, BigDecimal money) {
+
+        if(Objects.equals(money, new BigDecimal(0))) {
+            return new BigDecimal(0);
+        }
+
+        BigDecimal exChangeComplete = null;
+
+        if(accountCurrency == AccountCurrency.USD) {
+            exChangeComplete = money.multiply(CommonConstant.wonDollar);
+        } else if (accountCurrency == AccountCurrency.EUR) {
+            exChangeComplete = money.multiply(CommonConstant.wonEuro);
+        } else if (accountCurrency == AccountCurrency.JPY) {
+            exChangeComplete = money.multiply(CommonConstant.wonJpy);
+        } else if (accountCurrency == AccountCurrency.KRW) {
+            exChangeComplete = money;
+        }
+
+        return exChangeComplete;
+    }
+
+    public BankAccount findBankAccountByAccountNum(String accountNumber) {
+        return em.createQuery("select ba from BankAccount ba where ba.accountNumber = :accountNumber", BankAccount.class)
+                .setParameter("accountNumber", accountNumber)
+                .getSingleResult();
     }
 }
