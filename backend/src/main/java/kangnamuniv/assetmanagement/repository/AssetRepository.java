80c228f3446130565926f2766fe9153c97e8db86
GunwooJo <kanggi1997@naver.com>
2024-05-05 19:48:48 +0900

    feat: 자산 저장메서드

---
+++ backend/src/main/java/kangnamuniv/assetmanagement/repository/AssetRepository.java
@@ -0,0 +1,31 @@
+package kangnamuniv.assetmanagement.repository;
+
+import jakarta.persistence.EntityManager;
+import kangnamuniv.assetmanagement.entity.Asset;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Repository
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class AssetRepository {
+
+    private final EntityManager em;
+
+    public void save(BigDecimal cash, BigDecimal stock_valuation, BigDecimal bond_valuation, BigDecimal property_valuation) {
+
+        Asset asset = Asset.builder()
+                .cash(cash)
+                .stockValuation(stock_valuation)
+                .bondValuation(bond_valuation)
+                .propertyValuation(property_valuation)
+                .build();
+
+        em.persist(asset);
+    }
+}
